fb1c9e24243baeb57e765ad798d6533aecaf821e
riewert <thesaurie@hotmail.com>
2019-09-29 16:36:33 +0200

    Revert "Merge remote-tracking branch 'Madcows/RazorsFrozen' into riewert"
    This reverts commit 86fe9700c25b55a006a2ec3edc0ef201ba3ed543.

--- Firmware/MotorControl/endstop.cpp
+++ Firmware/MotorControl/endstop.cpp
@@ -2,22 +2,22 @@
 
 Endstop::Endstop(Endstop::Config_t& config)
     : config_(config) {
-    update_config();
+    set_endstop_enabled(config_.enabled);
 }
 
 void Endstop::update() {
-    uint16_t gpio_pin = get_gpio_pin_by_pin(config_.gpio_num);
+    uint16_t gpio_pin       = get_gpio_pin_by_pin(config_.gpio_num);
     GPIO_TypeDef* gpio_port = get_gpio_port_by_pin(config_.gpio_num);
-    bool last_pin_state = pin_state_;
-    pin_state_ = HAL_GPIO_ReadPin(gpio_port, gpio_pin);
-    float now = axis_->loop_counter_ * current_meas_period;
+    auto last_pin_state     = pin_state_;
+    pin_state_              = HAL_GPIO_ReadPin(gpio_port, gpio_pin);
     if (pin_state_ != last_pin_state) {
-        debounce_timer_ = now;
+        debounce_timer_ = axis_->loop_counter_ * current_meas_period;
     }
     if (config_.enabled) {
-        if ((now - debounce_timer_) >= (config_.debounce_ms * 0.001f)) {         // Debounce timer expired, take the new pin state
-            endstop_state_ = config_.is_active_high ? pin_state_ : !pin_state_;  // endstop_state is the logical state
-            debounce_timer_ = now - (config_.debounce_ms * 0.001f);              // Ensure timer doesn't have overflow issues
+        float now = axis_->loop_counter_ * current_meas_period;
+        if ((now - debounce_timer_) >= (config_.debounce_ms * 0.001f)) {          // Debounce timer expired, take the new pin state
+            endstop_state_  = config_.is_active_high ? pin_state_ : !pin_state_;  // endstop_state is the logical state
+            debounce_timer_ = now - (config_.debounce_ms * 0.001f);               // Ensure timer doesn't have overflow issues
         } else {
             endstop_state_ = endstop_state_;  // Do nothing
         }
@@ -26,22 +26,22 @@ void Endstop::update() {
     }
 }
 
-bool Endstop::get_state() {
+bool Endstop::getEndstopState() {
     return endstop_state_;
 }
 
-void Endstop::update_config(){
-    set_enabled(config_.enabled);
+void Endstop::update_endstop_config() {
+    set_endstop_enabled(config_.enabled);
 }
 
-void Endstop::set_enabled(bool enable) {
+void Endstop::set_endstop_enabled(bool enable) {
     if (config_.gpio_num != 0) {
-        uint16_t gpio_pin = get_gpio_pin_by_pin(config_.gpio_num);
+        uint16_t gpio_pin       = get_gpio_pin_by_pin(config_.gpio_num);
         GPIO_TypeDef* gpio_port = get_gpio_port_by_pin(config_.gpio_num);
         if (enable) {
             HAL_GPIO_DeInit(gpio_port, gpio_pin);
             GPIO_InitTypeDef GPIO_InitStruct;
-            GPIO_InitStruct.Pin = gpio_pin;
+            GPIO_InitStruct.Pin  = gpio_pin;
             GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
             GPIO_InitStruct.Pull = config_.is_active_high ? GPIO_PULLDOWN : GPIO_PULLUP;
             HAL_GPIO_Init(gpio_port, &GPIO_InitStruct);
